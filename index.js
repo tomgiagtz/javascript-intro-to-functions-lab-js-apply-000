c134b8698daac09d255679a8f7bcf362d30e07e7
Tom Giagtzoglou <tom.giagtzoglou@gmail.com>
2018-08-05 19:11:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,5 +7,23 @@ function whisper(string) {
 }
 
 function logShout(string) {
-  console.log(string.toUpperCase);
+  console.log(string.toUpperCase)
+}
+
+function logWhisper(string) {
+  console.log(string.toLowerCase)
+}
+
+function sayHiToGrandma(string) {
+  var upper = shout(string)
+  var lower = whisper(string)
+  var love = "I love you, Grandma."
+  
+  if (string === upper) {
+    return "YES INDEED !"
+  } else if (string === lower) {
+    return "I can't hear you!"
+  } else if (string === love) {
+    return "I love you, too."
+  }
 }
